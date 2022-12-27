8fcff22c8073dc1c89c118215db977f833bf7231
william.ridge.95 <william.ridge.95@gmail.com>
2022-12-27 15:29:27 +1100

    clean ignore

--- contracts/pixels.sol
+++ contracts/pixels.sol
@@ -1,13 +1,20 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.17;
+pragma solidity 0.8.17;
 
-import { ERC721 } from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import { SafeMath } from "@openzeppelin/contracts/utils/math/SafeMath.sol";
-import { Base64 } from "@openzeppelin/contracts/utils/Base64.sol";
-import { Strings } from "@openzeppelin/contracts/utils/Strings.sol";
+import {SafeMath} from "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import {Base64} from "@openzeppelin/contracts/utils/Base64.sol";
+import {Strings} from "@openzeppelin/contracts/utils/Strings.sol";
 
 import "hardhat/console.sol";
 
 contract pixels {
-    
+    using SafeMath for uint256;
+
+    //variables
+
+    //events
+
+    //constructor
+
+    //functions
 }
